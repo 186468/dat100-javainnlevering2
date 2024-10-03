28de338ba695f064a45b8c5b4807b34f0215b030
nicolaijo10 <nuster200569@gmail.com>
2024-10-03 13:33:37 +0200

    Matrise oppgave C og D

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -50,19 +50,34 @@ public class Matriser {
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
-	}
+        
+        int[][] nyMatrise = new int[matrise.length][matrise[0].length];
+               
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {                
+                nyMatrise[i][j] = matrise[i][j] * tall;
+            }
+        }                
+        return nyMatrise;
+    }
 
 	// d)
-	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
-	}
+	public static boolean erLik(int[][] mat1, int[][] mat2) {
+        
+        if (mat1.length != mat2.length || mat1[0].length != mat2[0].length) {
+            return false;
+        }
+                
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[i].length; j++) {                
+                if (mat1[i][j] != mat2[i][j]) {
+                    return false; 
+                }
+            }
+        }
+                
+        return true;
+    }
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
